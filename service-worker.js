d37c336083546e3fa4b59afec12080b835ab9598
Anurag Verma <anurag.verma.ug20@nsut.ac.in>
2023-07-21 12:05:58 +0530

    new line fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54c80bcfc1e60b32aaf9b90769fc81f6.js"
+  "/precache-manifest.83ca66a16d0186550b779323f92d0dba.js"
 );
 
 self.addEventListener('message', (event) => {
