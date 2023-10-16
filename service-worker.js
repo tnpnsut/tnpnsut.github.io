436768a3a990051ab0140f6a49a3385608c85d13
Anurag Verma <anurag.verma.ug20@nsut.ac.in>
2023-10-16 10:10:33 +0530

    fix null errors

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6a42a85e84d7e9805b5ac4141ddf2c2.js"
+  "/precache-manifest.f8a0367c846d4ca8d2530ae9eec44aa4.js"
 );
 
 self.addEventListener('message', (event) => {
